73a783aa7a127c3dea059bfb064c9d1f58a687ff
davide-sketch <120529757+davide-sketch@users.noreply.github.com>
2026-06-27 16:14:16 +0200

    User Story 3 completata

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,31 @@ use App\Http\Controllers\PublicController;
 use App\Http\Controllers\RevisorController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
-Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
-Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
 Route::get('/article/index', [ArticleController::class, 'index'])->name('article.index');
 Route::get('/article/show/{article}', [ArticleController::class, 'show'])->name('article.show');
 Route::get('/article/category/{category}', [ArticleController::class, 'byCategory'])->name('article.byCategory');
 Route::get('/careers', [PublicController::class, 'careers'])->name('careers');
 Route::post('/careers/submit', [PublicController::class, 'careersSubmit'])->name('careers.submit');
-Route::middleware('admin')->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+
+Route::middleware('writer')->group(function () {
+    Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
+    Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
 });
+
+
+Route::middleware('revisor')->group(function () {
+    Route::get('/revisor/dashboard', [RevisorController::class, 'dashboard'])->name('revisor.dashboard');
+    Route::patch('/accept/article/{article}', [RevisorController::class, 'acceptArticle'])->name('revisor.acceptArticle');
+    Route::patch('/reject/article/{article}', [RevisorController::class, 'rejectArticle'])->name('revisor.rejectArticle');
+    Route::patch('/undo/article/{article}', [RevisorController::class, 'undoArticle'])->name('revisor.undoArticle');
+});
+
+
 Route::middleware('admin')->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
     Route::patch('/admin/set-admin/{user}', [AdminController::class, 'setAdmin'])->name('admin.setAdmin');
     Route::patch('/admin/set-revisor/{user}', [AdminController::class, 'setRevisor'])->name('admin.setRevisor');
     Route::patch('/admin/set-writer/{user}', [AdminController::class, 'setWriter'])->name('admin.setWriter');
-});
-Route::middleware('revisor')->group(function () {
-    Route::get('/revisor/dashboard', [RevisorController::class, 'dashboard'])->name('revisor.dashboard');
-});
-Route::middleware('revisor')->group(function () {
-    // ...
-    Route::patch('/accept/article/{article}', [RevisorController::class, 'acceptArticle'])->name('revisor.acceptArticle');
-    Route::patch('/reject/article/{article}', [RevisorController::class, 'rejectArticle'])->name('revisor.rejectArticle');
-    Route::patch('/undo/article/{article}', [RevisorController::class, 'undoArticle'])->name('revisor.undoArticle');
 });
